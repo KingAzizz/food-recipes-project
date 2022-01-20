350174c6caae0ef7cca0c7c3fb1f273e86b63974
Abdulaziz alsunaydi <azoz@test.com>
2022-01-20 23:31:06 +0300

    build the cards and make a component to reuse them in ListsRecipes component

---
+++ src/components/Recipes.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+import "../styles/Recipes.css";
+const Recipes = ({ displayName, ingSteps, img }) => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <section className="card-container">
+      <h2 onClick={() => setShow(!show)}>{displayName}</h2>
+      <div className="img-conatiner">
+        {img === "" ? (
+          ""
+        ) : (
+          <img
+            className={`${show ? "img-photo" : "img-photo-hide"}`}
+            src={img}
+            alt="food recipe"
+          />
+        )}
+      </div>
+      <br />
+      <ol className={`${show ? "order-list-reveal" : "order-list"}`}>
+        {ingSteps.map((array, index) => (
+          <li key={index}>{array}</li>
+        ))}
+      </ol>
+    </section>
+  );
+};
+
+export default Recipes;
