f265314729d944fa4913dd09c36c357a7103726a
Abdulaziz alsunaydi <azoz@test.com>
2022-01-22 20:30:04 +0300

    small change if the ingSteps is undefined or not an array it will not show the card that has recipe info and handle error in the search components if it returns undefined it will not break the site it will show h2 tag says sorry we couldn't find what you are looking for

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -3,28 +3,30 @@ import { useState } from "react";
 import "../styles/Recipes.css";
 const Recipes = ({ displayName, ingSteps, img }) => {
   const [show, setShow] = useState(false);
-
-  return (
-    <section className="card-container">
-      <h2 onClick={() => setShow(!show)}>{displayName}</h2>
-      <div className="img-conatiner">
-        {img === "" ? (
-          ""
-        ) : (
-          <img
-            className={`${show ? "img-photo" : "img-photo-hide"}`}
-            src={img}
-            alt="food recipe"
-          />
-        )}
-      </div>
-      <br />
-      <ol className={`${show ? "order-list-reveal" : "order-list"}`}>
-        {ingSteps.map((array, index) => (
-          <li key={index}>{array}</li>
-        ))}
-      </ol>
-    </section>
+  return (<>
+  {ingSteps === undefined? <h2>Sorry we couldn't find anything related to your search</h2> :<section className="card-container">
+    <h2 onClick={() => setShow(!show)}>{displayName}</h2>
+    <div className="img-conatiner">
+      {img === "" ? (
+        ""
+      ) : (
+        <img
+          className={`${show ? "img-photo" : "img-photo-hide"}`}
+          src={img}
+          alt="food recipe"
+        />
+      )}
+    </div>
+    <br />
+    
+    <ol className={`${show ? "order-list-reveal" : "order-list"}`}>
+      {ingSteps.map((array, index) => (
+        <li key={index}>{array}</li>
+      ))}
+    </ol>
+    
+  </section>}
+    </>
   );
 };
 
