0c2832835e93fa4b28e790d9de5e61dcf61789bc
Abdulaziz alsunaydi <azoz@test.com>
2022-01-22 20:42:43 +0300

    a new Route for Search Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Alert from './components/Alert';
 import ListsRecipes from './components/ListsRecipes';
 import Navbar from './components/Navbar';
+import Search from './components/Search';
+
 function App() {
   
   return (
       <Router>
     <div className="App">
       <Navbar />
+      <Alert />
     <Routes> 
     
     <Route path="/" element={<ListsRecipes />} />
+    <Route path="/search" element={<Search />} />
 
     </Routes>
     </div>
