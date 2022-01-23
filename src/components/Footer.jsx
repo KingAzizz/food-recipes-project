6e28a80d8bb17900c30c2b9552322b16edc2c1df
Abdulaziz alsunaydi <azoz@test.com>
2022-01-23 23:29:43 +0300

    add a footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return <div className="footer-container">Made With ❤️ Aziz 
+        <ul>
+        <li> <a className="a-footer" href="https://twitter.com/xilAziz"><i className="fab fa-twitter"></i></a></li>
+        <li> <a className="a-footer" href="https://github.com/KingAzizz"><i className="fab fa-github"></i></a></li>
+      </ul>
+      </div>;
+};
+
+export default Footer;
