7c37e28c1f40f51ca6ddde1187c098ff5d541542
PietroLinguini <rayhmiot@gmail.com>
2022-02-12 12:55:53 -0500

    Changed es5 functions to es6 arrow functions
    Just had to change one function, the rest of them were arrow functions already

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 app.set(cors())
 
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
